68004b33b6910373664bb585bf77bbc7744e59a9
Molina0127 <guimoltri@gmail.com>
2024-08-16 15:13:29 -0300

    Finalizando o JavaScript

--- Projeto/script.js
+++ Projeto/script.js
@@ -1,12 +1,17 @@
 function toggleMode() {
-    
     const html = document.documentElement
+    html.classList.toggle('light')
     
+    const img = document.querySelector("#profile img")
+
     if(html.classList.contains('light')) {
-        html.classList.remove('light')
+        img.setAttribute("src", "./assets/avatar_guilherme_molina.png")
+        img.setAttribute("alt", "Foto de Guilherme Molina, sorri com os lábios, olhos castanhos escuros, rosto fino, barba e cabelo curto, veste uma camisa polo branca, fundo cinza.")
     }
     else {
-        html.classList.add('light')
+        img.setAttribute("src", "./assets/avatar_mayk_brito.png")
+        img.setAttribute("alt", "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo.")
     }
+    
 
 }
