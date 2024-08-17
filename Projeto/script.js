626116f0b8bf2a8e745309aed03d202ca1c302bd
Molina0127 <guimoltri@gmail.com>
2024-08-17 14:37:42 -0300

    Adicionando animacoes com CSS

--- Projeto/script.js
+++ Projeto/script.js
@@ -8,10 +8,10 @@ function toggleMode() {
         img.setAttribute("src", "./assets/avatar_guilherme_molina.png")
         img.setAttribute("alt", "Foto de Guilherme Molina, sorri com os lábios, olhos castanhos escuros, rosto fino, barba e cabelo curto, veste uma camisa polo branca, fundo cinza.")
     }
-    else {
-        img.setAttribute("src", "./assets/avatar_mayk_brito.png")
-        img.setAttribute("alt", "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo.")
-    }
+    // else {
+    //     img.setAttribute("src", "./assets/avatar_mayk_brito.png")
+    //     img.setAttribute("alt", "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo.")
+    // }
     
 
 }
